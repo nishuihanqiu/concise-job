7340f422bb03893ce3a6223d3c216be13b90a732
li-liangshan <liliangshanlls@gmail.com>
2018-12-23 21:43:55 +0800

    a little

--- concise-core/src/main/java/com/lls/api/concise/rpc/JettyRemoteServer.java
+++ concise-core/src/main/java/com/lls/api/concise/rpc/JettyRemoteServer.java
@@ -1,11 +1,28 @@
 package com.lls.api.concise.rpc;
 
+import com.lls.api.concise.core.Configuration;
+import com.lls.api.concise.core.Registry;
+import com.lls.api.concise.core.RegistryConfig;
+import com.lls.api.concise.logging.Logger;
+import com.lls.api.concise.logging.LoggerFactory;
+import com.lls.api.concise.worker.WorkerContext;
+
 /************************************
  * JettyRemoteServer
  * @author liliangshan
  * @date 2018/12/23
  ************************************/
 public class JettyRemoteServer extends AbstractRemoteServer {
+
+    private static final Logger logger = LoggerFactory.getLogger(JettyRemoteServer.class);
+    private Registry registry;
+    private RegistryConfig registryConfig;
+    private Configuration configuration;
+    private WorkerContext workerContext;
+
+    public JettyRemoteServer() {
+        workerContext = new WorkerContext();
+    }
 
     @Override
     protected void beforeServerStart() {
